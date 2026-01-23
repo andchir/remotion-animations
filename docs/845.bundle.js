7a8d34e8861fbfb2ea89e98ff2d1f159101b4438
konard <drakonard@gmail.com>
2026-01-23 11:55:41 +0100

    Add GitHub Pages deployment workflow and update build script
    - Modify package.json build script to output to docs/ directory
    - Add .github/workflows/deploy.yml for automated GitHub Pages deployment
    - Build and commit static site to docs/ for GitHub Pages hosting

---
+++ docs/845.bundle.js
@@ -0,0 +1,11 @@
+(self["webpackChunkremotion_animations"] = self["webpackChunkremotion_animations"] || []).push([[845],{
+
+/***/ 1845:
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ })
+
+}]);
+//# sourceMappingURL=845.bundle.js.map
